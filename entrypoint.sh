f8d0458ba3d23d421eea250eda6c68a44c5f1b53
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-01-05 04:22:17 +0400

    Reorganization of the repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,13 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-pushd pkg/aur > /dev/null
-sed -i "s/^pkgver=.*\$/pkgver=${GITHUB_REF/refs\/tags\/k/}/" "$1"/PKGBUILD
 cp -r "$1" /home/builduser
-popd > /dev/null
 
 pushd /home/builduser > /dev/null
 chown -R builduser:builduser "$1"
 
 pushd "$1" > /dev/null
-su -c updpkgsums builduser
-su -c '. PKGBUILD; yay -S --noconfirm ${makedepends[@]} ${depends[@]}' builduser
+su -c '. PKGBUILD && yay -S --noconfirm ${makedepends[@]} ${depends[@]}' builduser
 su -c makepkg builduser
 popd > /dev/null
 
