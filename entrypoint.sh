0549e20e009e06c1e01fefc80a34328c9004e5d5
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-01-06 16:24:56 +0400

    Autobuild package for release

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,9 @@ chown -R builduser:builduser "$1"
 pushd "$1" > /dev/null
 su -c '. PKGBUILD && yay -S --noconfirm ${makedepends[@]} ${depends[@]}' builduser
 su -c makepkg builduser
+cp "$1"-*.pkg.* /github/workspace
+popd > /dev/null
 popd > /dev/null
 
-popd > /dev/null
+PACKAGE_FILE="$(echo "$1"-*.pkg.*)"
+echo ::set-output name=package-file::"$PACKAGE_FILE"
